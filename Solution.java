cbfd1e27b29a7e3592b4eb983ced7805bacc5765
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-11 14:03:59 -0700

    first solution attempt, faster than 95.3% users, less memory usage than 28.62%

--- Solution.java
+++ Solution.java
@@ -1,28 +1,26 @@
-import java.util.*;
-
 public class Solution {
 
     char[] symbols = new char[] { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
-    int[] values = new int[] { 1, 5, 10, 100, 500, 1000 };
+    int[] values = new int[] { 1, 5, 10, 50, 100, 500, 1000 };
 
     public int romanToInt(String s) {
         
         int value = 0;
 
         for (int index = 0; index < s.length(); index++) {
-            if ((index + 1) >= s.length())
-                continue;
-
             char currentChar = s.charAt(index);
-            char nextChar = s.charAt(index + 1);
-
             int currentCharIndex = indexOf(currentChar);
-            int nextCharIndex = indexOf(nextChar);
-
-            if (nextCharIndex > currentCharIndex) {
-                int addVal = -values[currentCharIndex];
-                value += addVal;
-            } else {
+            if ((index + 1) < s.length()) {
+                char nextChar = s.charAt(index + 1);
+                int nextCharIndex = indexOf(nextChar);
+                if (nextCharIndex > currentCharIndex) {
+                    int addVal = -values[currentCharIndex];
+                    value += addVal;
+                } else {
+                    value += values[currentCharIndex];
+                }
+            } 
+            else {
                 value += values[currentCharIndex];
             }
         }
